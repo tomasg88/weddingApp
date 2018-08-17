e375d697210d1d8804dc1ec411340423883dd02a
Tomas <tomasg88@gmail.com>
2018-08-17 20:03:28 -0300

    AWS DB scripts

---
+++ node-express/app/local-db/db-scripts/create.js
@@ -0,0 +1,26 @@
+var AWS = require("aws-sdk");
+AWS.config.update({
+    region: "us-east-2",
+    endpoint: "http://localhost:8000"
+});
+var dynamodb = new AWS.DynamoDB();
+var params = {
+    TableName : "Usuarios",
+    KeySchema: [
+        { AttributeName: "id", KeyType: "HASH"},  //Partition key
+],
+    AttributeDefinitions: [
+        { AttributeName: "id", AttributeType: "N" },
+],
+    ProvisionedThroughput: {
+        ReadCapacityUnits: 5,
+        WriteCapacityUnits: 5
+    }
+};
+dynamodb.createTable(params, function(err, data) {
+    if (err) {
+        console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+    }
+});
